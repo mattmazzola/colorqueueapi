e9a49fe74d7c61ec9fd6e005e114551fc468bec9
Matt Mazzola <mattmazzola@live.com>
2016-04-25 22:53:07 -0700

    Add custom authentication for server to server communication using firebase-token-generator. Fix error handling.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import * as restify from 'restify';
 import Firebase = require('firebase');
+import FirebaseTokenGenerator = require('firebase-token-generator');
+
+const configJson = fs.readFileSync(path.join(__dirname, '..', 'config.json'), 'utf8');
+const config = JSON.parse(configJson);
+const tokenGenerator = new FirebaseTokenGenerator(config.firebaseSecret);
+const token = tokenGenerator.createToken({ uid: config.firebaseUid, version: '1.0' });
 
 const firebaseRef = new Firebase("https://colorqueue.firebaseio.com");
+firebaseRef.authWithCustomToken(token, (error, authData) => {
+  if (error) {
+    throw error;
+  }
+  
+  console.log("Login Succeeded!", authData);
+});
 
 const server = restify.createServer({
   name: 'ColorQueue'
@@ -29,17 +44,29 @@ server.post('/colors', function create(req, res, next) {
   const {r, g, b, a} = req.body;
   const timestamp = (new Date()).getTime();
   
-  firebaseRef.child('colors').push({r, g, b, a, order: timestamp});
-  
-  res.send(200);
-  return next();
+  firebaseRef.child('colors').push({r, g, b, a, order: timestamp}, (error) => {
+    if(error) {
+      res.send(500, error);
+    }
+    else {
+      res.send(200);
+    }
+    
+    return next();
+  });
 });
 
 server.post('/clear', function clear(req, res, next) {
-  firebaseRef.child('colors').set([]);
-  
-  res.send(200);
-  return next();
+  firebaseRef.child('colors').set([], (error) => {
+    if(error) {
+      res.send(500, error);
+    }
+    else {
+      res.send(200);
+    }
+    
+    return next();
+  });
 });
 
 server.listen(8080);
