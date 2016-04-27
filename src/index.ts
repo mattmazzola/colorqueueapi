ea3b43c866f178636654d5fbcd1f9c244eb71c81
Matt Mazzola <mattmazzola@live.com>
2016-04-26 23:24:33 -0700

    Remove server read since client has direct correction to firebase.

--- src/index.ts
+++ src/index.ts
@@ -26,20 +26,6 @@ server.use(restify.CORS());
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.get('/colors', function read(req, res, next) {
-  firebaseRef.child('colors').on('value', (dataSnapshot) => {
-    const firebaseColors = dataSnapshot.val();
-    const colors = Object.keys(firebaseColors)
-      .reduce((a, b) => {
-        a.push(firebaseColors[b]);
-        return a;
-      }, []);
-      
-    res.send(200, colors);
-    return next();
-  });
-});
-
 server.post('/colors', function create(req, res, next) {
   const {r, g, b, a} = req.body;
   const timestamp = (new Date()).getTime();
