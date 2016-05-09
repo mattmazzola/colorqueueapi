0550220e46e51f89378b692ead800e6bc3af5c66
Matt Mazzola <mattmazzola@live.com>
2016-05-08 21:29:56 -0700

    Add support for color removal via DELETE /colors/:id

--- src/index.ts
+++ src/index.ts
@@ -29,8 +29,17 @@ server.use(restify.bodyParser());
 server.post('/colors', function create(req, res, next) {
   const {r, g, b, a} = req.body;
   const timestamp = (new Date()).getTime();
+  const body = {
+    r,
+    g,
+    b,
+    a,
+    order: timestamp,
+    transition: 'linear',
+    duration: 30000
+  };
   
-  firebaseRef.child('colors').push({r, g, b, a, order: timestamp}, (error) => {
+  firebaseRef.child('colors').push(body, (error) => {
     if(error) {
       res.send(500, error);
     }
@@ -39,6 +48,46 @@ server.post('/colors', function create(req, res, next) {
     }
     
     return next();
+  });
+});
+
+server.del('/colors/:id', function create(req, res, next) {
+  const colorId = parseInt(req.params.id);
+  
+  if(typeof colorId !== 'number') {
+    res.send(400, `id must be a number. You passed ${req.params.id}`);
+    return next();
+  }
+  
+  firebaseRef.child('colors').orderByChild('order').once('value', (dataSnapshot) => {
+    const colorsHash = dataSnapshot.val();
+    let color = undefined;
+    
+    Object.keys(colorsHash)
+      .some(key => {
+        const c = colorsHash[key];
+        c.key = key;
+        
+        if(c.order === colorId) {
+          color = c;
+          return true;
+        }
+      });
+
+    if(!color) {
+      res.send(400, `No color found with id: ${colorId}.`);
+      return next();
+    }
+    
+    return firebaseRef.child('colors').child(color.key).remove()
+      .then(() => {
+        res.send(200);
+        return next();
+      })
+      .catch(error => {
+        res.send(500, error);
+        return next();
+      });
   });
 });
 
